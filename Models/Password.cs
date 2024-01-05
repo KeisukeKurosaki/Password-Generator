e6cf30002b564c7c69aaac4a3a2ef0fffd329154
KeisukeKurosaki <Cglanville4645@conestogac.on.ca>
2024-01-05 00:09:54 -0500

    Added more comments to the model class. Will need to make changes to the model class to fit more in line with the MVVM pattern to instead use properties instead of parameters.

--- Models/Password.cs
+++ Models/Password.cs
@@ -10,15 +10,33 @@ namespace Password_Generator
 {
     internal class Password
     {
+        // Used to hold the randomly generated password
         public string ActualPassword { get; set; }
 
+        // Used to determine if letters are checked by the user through data binding
         public bool LettersChecked { get; set; }
 
+        // Used to determine if symbols are checked by the user through data binding
         public bool SymbolsChecked { get; set; }
 
+        // Used to determine if symbols are checked by the user through data binding
         public bool NumbersChecked { get; set; }
 
-
+        /*
+        *	METHOD          : CreatePassword(bool symbols, bool letters, bool numbers)
+        *	DESCRIPTION		:
+        *		This method is used in order to select and determine which options should be included in the 
+        *		generation of a new password. As such, a bit mask will be used to navigate through the separate
+        *		options of which possibilities should be included in the string that will be randomized and ultimately 
+        *		generate the password. Each separate switch case statement is a different combination of the options 
+        *		available for genrating a unique password.
+        *	PARAMETERS		:
+        *		bool symbols       :   This bool indicates if symbols should be included in the password
+        *		bool letters       :   This bool indicates if letters should be included in the password
+        *		bool numbers       :   This bool indicates if numbers should be included in the password
+        *	RETURNS			:
+        *		void	           :   This method does not have any return value
+        */
         public void CreatePassword(bool symbols, bool letters, bool numbers)
         {
             string randomizationString;
@@ -28,52 +46,62 @@ namespace Password_Generator
             switch (bitmask)
             {
                 case 1: // Only Symbols selected
-
                     randomizationString = Constants.kSymbols;
                     Randomize(randomizationString, Constants.kPasswordLength);
                     break;
+
                 case 2: // Only Letters selected
 
                     randomizationString = Constants.kLetters;
                     Randomize(randomizationString, Constants.kPasswordLength);
                     break;
-                case 3: // Symbols and Letters selected
 
+                case 3: // Symbols and Letters selected
                     randomizationString = Constants.kSymbols + Constants.kLetters;
                     Randomize(randomizationString, Constants.kPasswordLength);
                     break;
-                case 4: // Only Numbers selected
 
+                case 4: // Only Numbers selected
                     randomizationString = Constants.kNumbers;
                     Randomize(randomizationString, Constants.kPasswordLength);
-
-
                     break;
-                case 5: // Symbols and Numbers selected
 
+                case 5: // Symbols and Numbers selected
                     randomizationString = Constants.kSymbols + Constants.kNumbers;
                     Randomize(randomizationString, Constants.kPasswordLength);
-
                     break;
-                case 6: // Letters and Numbers selected
 
+                case 6: // Letters and Numbers selected
                     randomizationString = Constants.kLetters + Constants.kNumbers;
                     Randomize(randomizationString, Constants.kPasswordLength);
-
                     break;
+
                 case 7: // Symbols, Letters, and Numbers selected
 
                     randomizationString = Constants.kLetters + Constants.kNumbers + Constants.kSymbols;
                     Randomize(randomizationString, Constants.kPasswordLength);
-
                     break;
             }
         }
 
+        /*
+        *	METHOD          : Randomize(string potentialOptions, int passwordLength)
+        *	DESCRIPTION		:
+        *		This method is used in order to randomize a string using the Fisher-Yates shuffle algorithm. This 
+        *		will be the method that will randomly generate new passwords depending on which options the user 
+        *		has selected (symbols, letters, numbers) to be included. Utilizing this algorithm helps in creating
+        *		a new password that is unbiased in the selection of the characters, letters, or symbols to be included.
+        *	PARAMETERS		:
+        *		string potentialOptions      :   The string containing the potential letters, symbols, or numbers
+        *		int passwordLength           :   The length of the password that will be made      
+        *	RETURNS			:
+        *		void	                     :   This method does not have any return value
+        */
         public void Randomize(string potentialOptions, int passwordLength)
         {
             Random rng = new Random();
 
+            // Take the string containing a combination or single option of letters, numbers, and symbols and convert it to a char[]
             char[] chars = potentialOptions.ToCharArray();
 
             // Shuffle the characters using Fisher-Yates (Knuth) Shuffle algorithm
@@ -85,9 +113,10 @@ namespace Password_Generator
                 chars[j] = temp;
             }
 
-            // Take the specified length of characters from the shuffled array
+            // Take the specified length of characters from the shuffled array and store them in a string
             string randomizedString = new string(chars, 0, passwordLength);
 
+            // Store the randomly generated password in the ActualPassword property of the model class
             ActualPassword = randomizedString;
         }
     }
